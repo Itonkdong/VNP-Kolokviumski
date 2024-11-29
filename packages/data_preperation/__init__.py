6aeb8fc3729bdfc83af3227d14c7a884fb700440
Itonkdong <kostadinoskiviktor@yahoo.com>
2024-11-29 22:27:07 +0100

    packages

---
+++ packages/data_preperation/__init__.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder
---
+++ packages/models/__init__.py
@@ -0,0 +1,20 @@
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.linear_model import LogisticRegression
+
+import xgboost as xgb
+
+from xgboost import XGBClassifier
+from xgboost import XGBRegressor
+
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score, cross_val_predict
+
+from sklearn.model_selection import GridSearchCV
+
