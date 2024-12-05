60c9f945fdf6fc14ade6017be97836aea66a9107
Itonkdong <kostadinoskiviktor@yahoo.com>
2024-12-05 23:52:25 +0100

    Time Series Functions and Some other usefull stuff

--- packages/vnp_ez/__init__.py
+++ packages/vnp_ez/__init__.py
@@ -7,6 +7,8 @@ from sklearn.impute import KNNImputer
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
 def missing_table(data: pd.DataFrame):
@@ -113,7 +115,34 @@ def impute_data(data: pd.DataFrame, data_to_impute: list, strategies: list):
     return data_copy
 
 
-def encode_data(data: pd.DataFrame, features_to_encode: list):
+def encode_data(data: pd.DataFrame, features_to_encode: list, strategy="ordinal", return_encoders=False):
+    encoders = {}
+    encoded_data = None
+    if strategy == "ordinal" or strategy == "label":
+        encoded_data, encoders = ordinal_encode_data(data, features_to_encode)
+    elif strategy == "onehot":
+        encoded_data, encoders = one_hot_encode_data(data, features_to_encode)
+
+    if return_encoders:
+        return encoded_data, encoders
+    else:
+        return encoded_data
+
+
+def one_hot_encode_data(data: pd.DataFrame, features_to_encode: list):
+    encoders = {}
+    data_copy = data.copy()
+    for feature in features_to_encode:
+        feature_flags = pd.get_dummies(data_copy[feature])
+        feature_flags = feature_flags.replace(True, 1)
+        feature_flags = feature_flags.replace(False, 0)
+        data_copy = data_copy.drop(columns=feature, axis=1)
+        data_copy = pd.concat([data_copy, feature_flags], axis=1)
+        encoders[feature] = list(feature_flags.columns)
+    return data_copy, encoders
+
+
+def ordinal_encode_data(data: pd.DataFrame, features_to_encode: list):
     encoders = {}
     data_copy = data.copy()
     for feature in features_to_encode:
@@ -123,6 +152,89 @@ def encode_data(data: pd.DataFrame, features_to_encode: list):
     return data_copy, encoders
 
 
+def scale_data(train, test, strategy="standard", features_to_scale=()):
+    train_copy = train.copy()
+    test_copy = test.copy()
+    if len(features_to_scale) == 0:
+        features_to_scale = get_numerical_features_names(train_copy)
+
+    scaler = None
+    if strategy == "standard":
+        scaler = StandardScaler()
+
+    elif strategy == "minmax":
+        scaler = MinMaxScaler()
+
+    scaler.fit(train_copy[features_to_scale])
+    train_copy[features_to_scale] = scaler.transform(train_copy[features_to_scale])
+    test_copy[features_to_scale] = scaler.transform(test_copy[features_to_scale])
+
+    return train_copy, test_copy
+
+
 def show_ba_displots_pairwise(data_before: pd.DataFrame, data_after: pd.DataFrame, features: list):
     for feature in features:
         show_displot_before_and_after_inputation(data_before, data_after, missing_feature_name=feature)
+
+
+def to_time_series(data: pd.DataFrame, time_feature, auto_sort=True):
+    data_copy = data.copy()
+    data_copy[time_feature] = pd.to_datetime(data_copy[time_feature])
+    data_copy = data_copy.set_index(time_feature)
+    if auto_sort:
+        return data_copy.sort_index()
+    return data_copy
+
+
+def correlation_map(data: pd.DataFrame, figsize=None):
+    if figsize is not None:
+        num_features = len(data.columns)
+        if num_features <= 8:
+            figsize = (12, 8)
+        elif num_features <= 15:
+            figsize = (20, 15)
+        else:
+            figsize = (28, 20)
+
+    plt.figure(figsize=figsize)
+    sns.heatmap(data[get_numerical_features_names(data)].corr(), annot=True)
+    plt.show()
+
+
+def auto_shift(data: pd.DataFrame, lag, features_to_shift: list, shifted_feature_name="*_prev_i", auto_drop_na=False,
+               return_features=True):
+    data_copy = data.copy()
+    for i in range(1, lag + 1):
+        for feature in features_to_shift:
+            if shifted_feature_name == "*_prev_i":
+                data_copy[f'{feature}_prev_{i}'] = data[feature].shift(i)
+            else:
+                data_copy[f'{shifted_feature_name}_{i}'] = data[feature].shift(i)
+
+    if auto_drop_na:
+        data_copy = data_copy.dropna(axis=0)
+
+    if return_features:
+        features = data_copy.columns.drop(features_to_shift)
+        return data_copy, list(features)
+    else:
+        return data_copy
+
+
+def regression_report(y_true, y_pred, include_mae=False):
+    print("Regression Report:")
+    mse = mean_squared_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
+    print(f"Mean Square Error: {mse}")
+    if include_mae:
+        mae = mean_absolute_error(y_true, y_pred)
+        print(f"Mean Absolute Error: {mae}")
+    print(f"R2 Score: {r2}")
+
+
+def show_time_series_predicts(y_test, y_pred, figsize=(20, 10)):
+    plt.figure(figsize=figsize)
+    plt.plot(y_test.values, label='actual')
+    plt.plot(y_pred, label='predicted')
+    plt.legend()
+    plt.show()
